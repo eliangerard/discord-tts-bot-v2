6f8d937386aed8a537165c40a67feb40e540aca0
Elian <eliangerardiso@gmail.com>
2022-09-16 13:30:21 -0600

    Added if alone disconnect event

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -5,7 +5,7 @@ module.exports = {
 	once: false,
 	async execute(oldState, newState, client) {
         const me = await newState.guild.members.fetchMe();
-        if(me.voice.channel == undefined || me.voice.channel != oldState.channel) return;
+        if(me.voice.channel == undefined || me.voice.channel != oldState.channel || me.voice.channel == newState.channel) return;
         console.log(oldState.channel.members.size);
 		if(oldState.channel.members.size > 1) return;
 
